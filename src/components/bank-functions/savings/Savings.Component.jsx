0ad1ee5bb9c8a0fbe9afcdbb7a7d2c0f2baf9692
walwizard <goneisgone@hotmail.com>
2023-04-05 17:03:39 -0300

    Bank App - SAvings Components

--- src/components/bank-functions/savings/Savings.Component.jsx
+++ src/components/bank-functions/savings/Savings.Component.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import "./Savings.Component.css";
 
+import { Link } from "react-router-dom";
+
 const SavingsComponent = () => {
   return (
     <div className="savings__component">
+      <div className="top__info">
+        <h1 className="top__h1">
+          <Link to={"/account"}>
+            <i class="bi bi-chevron-left"></i>
+          </Link>
+          Savings
+        </h1>
+      </div>
+
       <img
         src={require("../../images/saves.jpg")}
         alt="saves"
@@ -13,14 +24,13 @@ const SavingsComponent = () => {
       <h1 className="savings__cash">$ 15,000.00</h1>
 
       <div className="op">
-          <h1 className="deposit">
-            <span className="plus">+</span><i class="fa-solid fa-coins"></i> &nbsp;
-            Cash Deposit
-          </h1>
-          <h1 className="withdraw">
-            <i class="bi bi-coin"></i>&nbsp;&nbsp;&nbsp;
-            Withdraw
-          </h1>
+        <h1 className="deposit">
+          <span className="plus">+</span>
+          <i class="fa-solid fa-coins"></i> &nbsp; Cash Deposit
+        </h1>
+        <h1 className="withdraw">
+          <i class="bi bi-coin"></i>&nbsp;&nbsp;&nbsp; Withdraw
+        </h1>
       </div>
     </div>
   );
