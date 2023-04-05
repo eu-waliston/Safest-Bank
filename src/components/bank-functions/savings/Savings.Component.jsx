684522fb4df0777d825e91b3076af15305573f1f
walwizard <goneisgone@hotmail.com>
2023-04-05 16:40:01 -0300

    Bank App - SAvings Components

--- src/components/bank-functions/savings/Savings.Component.jsx
+++ src/components/bank-functions/savings/Savings.Component.jsx
@@ -2,11 +2,28 @@ import React from "react";
 import "./Savings.Component.css";
 
 const SavingsComponent = () => {
-    return (
-        <div>
+  return (
+    <div className="savings__component">
+      <img
+        src={require("../../images/saves.jpg")}
+        alt="saves"
+        className="saves"
+      />
+      <h1 className="savings__h1">SAvings Account</h1>
+      <h1 className="savings__cash">$ 15,000.00</h1>
 
-        </div>
-    )
-}
+      <div className="op">
+          <h1 className="deposit">
+            <span className="plus">+</span><i class="fa-solid fa-coins"></i> &nbsp;
+            Cash Deposit
+          </h1>
+          <h1 className="withdraw">
+            <i class="bi bi-coin"></i>&nbsp;&nbsp;&nbsp;
+            Withdraw
+          </h1>
+      </div>
+    </div>
+  );
+};
 
 export default SavingsComponent;
